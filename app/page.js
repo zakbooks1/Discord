bb61884943684481d6937d14b723b22ecb2c58f1
zakbooks1 <zak.books1@gmail.com>
2026-03-18 10:18:29 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,13 +11,12 @@ export default function Home() {
   const [tempPass, setTempPass] = useState("");
   const scrollRef = useRef(null);
 
-  const APP_ADMIN_PASS = "67bits"; // MUST match the backend
+  const APP_ADMIN_PASS = "67bits67"; // MUST match the backend
 
   const servers = [
     { id: "general", name: "G", color: "#5865f2" },
     { id: "gaming", name: "🎮", color: "#3ba55c" },
-    { id: "coding", name: "💻", color: "#eb459e" }
-    { id: "the67s", name: "😋", color: "#fffff" }
+    { id: "coding", name: "💻", color: "#eb459e" },
   ];
 
   useEffect(() => {
