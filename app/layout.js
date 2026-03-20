09a7c4c640048117d2f756a11432a7345e3a5d72
zakbooks1 <zak.books1@gmail.com>
2026-03-20 07:34:32 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,10 @@
 // app/layout.js
-export const metadata = {
-  title: 'Discord Clone',
-  description: 'MongoDB Chat App',
-}
+import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
