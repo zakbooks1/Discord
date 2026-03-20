6fd02613c471957a1df72890457d5fea193fa22e
zakbooks1 <zak.books1@gmail.com>
2026-03-20 07:36:21 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,16 @@
-// app/layout.js
 import './globals.css'
+
+export const metadata = {
+  title: 'zakcord',
+  description: 'Created by Zak',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0, background: '#313338' }}>
+        {children}
+      </body>
     </html>
   )
 }
